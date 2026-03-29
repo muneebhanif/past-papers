069a27382949ee8998b5e64f1838483cd5913832
muneebhanif <muneebhanif@users.noreply.github.com>
2026-03-29 15:13:04 +0500

    new

--- src/lib/adminPath.js
+++ src/lib/adminPath.js
@@ -3,6 +3,6 @@ const rawPath = (import.meta.env.VITE_ADMIN_PANEL_PATH ?? "")
   .replace(/^\/+|\/+$/g, "");
 
 export const ADMIN_PANEL_PATH =
-  rawPath.length >= 12 ? rawPath : "control-room-ax7k9p2m";
+  rawPath.length > 0 ? rawPath : "control-room-ax7k9p2m";
 
 export const ADMIN_PANEL_ROUTE = `/${ADMIN_PANEL_PATH}`;
